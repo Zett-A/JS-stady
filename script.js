7a2fd286f90e68b61ee1c1d60740cfc6c77ee738
Jarl <kosianlucky@yandex.ru>
2021-07-06 16:16:24 +0300

    new code and addition

--- script.js
+++ script.js
@@ -23,3 +23,29 @@
 // alert(appData.budget / 30);
 
 //это первоначальный код до оптимизациии и внесения в него изменений с помощью цикла, условий и прочего
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    
+let appData = {
+	budget: money,
+	expenses: {'a2' : 'a4'},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+    };
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+// начинаем прописывать цикл, что не спрашивать каждый раз
+
+
+
+alert(appData.budget / 30);
